79678858ac77c397a3cdbf99cd49604a584ed6e9
Samuel Kubinský <kubinsky.s@gmail.com>
2024-06-10 17:32:04 +0200

    Initial Commit

---
+++ feed-layout/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  feed-layout
+//
+//  Created by Samuel Kubinský on 10/06/2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
